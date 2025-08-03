678bd39f0ed4a0be09158e6efbea39ccd1d8b199
Arya T S <aryats1299@gmail.com>
2025-08-04 03:39:08 +0530

    Update and rename 9_capitalize.py to capitalize.py

--- py_pgms/capitalize.py
+++ py_pgms/capitalize.py
@@ -1,10 +1,9 @@
 string1=input("Enter string: ")
 i =0
+
 while i<len(string1):
     if i !=2:
         print(string1[i],end='')
     else:
         print(string1[i].capitalize(),end='')
-    i+=1
-
-    
+    i+=1    
