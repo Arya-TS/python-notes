632edda90d40d9761a326e1665eff8c6321f052e
Arya T S <aryats1299@gmail.com>
2025-08-07 00:37:41 +0530

    Update string_palindrome.py

--- py_pgms/string_palindrome.py
+++ py_pgms/string_palindrome.py
@@ -1,7 +1,5 @@
 string1 = input("Enter a string: ")
 
-''''''
-
 for i in range(len(string1)//2):
     if string1[i] != string1[len(string1)-i-1]:
         print("Not")
@@ -9,15 +7,3 @@ for i in range(len(string1)//2):
 
 else:
     print("Pal")
-''''''
-
-'''new = ''
-for i in string1:
-    # i = 'p' -> new = 'p' +'' , i = 'y' -> new = 'y' + 'p', i = 't' -> new = 't' + 'yp' ...
-    new = i + new
-
-if string1 == new:
-    print("Pal")
-
-else:
-    print("Not")'''
